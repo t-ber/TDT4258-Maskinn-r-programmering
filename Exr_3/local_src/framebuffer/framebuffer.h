88490ad7f84be1e8131ae4ccd26a85dc7a9a24c9
stianss1 <47594604+stianss1@users.noreply.github.com>
2020-11-11 18:32:37 +0100

    framebuffer
    framebuffer.c and framebuffer.h

---
+++ Exr_3/local_src/framebuffer/framebuffer.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <linux/fb.h>
+#include <sys/ioctl.h>
+
+
+#define BLACK	0x0000
+#define GREEN	0x37E0
+#define RED		0xF800
+
+#define BLOCK_SIZE 10
+
+
+
+int initialize_screen()
+void clear_screen()
+void update_screen()
+void draw_square(int pos_x, int pos_y, int color_val)
+void draw_test(int pos_x, int pos_y)
